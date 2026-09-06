5bb871e9c702903d583307ce318461121c171f86
rtraselbd <rtraselbd72735@gmail.com>
2026-09-06 10:29:17 +0600

    chore: remove composer-based configuration from rector

--- rector.php
+++ rector.php
@@ -22,7 +22,6 @@ return RectorConfig::configure()
     ->withImportNames(
         removeUnusedImports: true,
     )
-    ->withComposerBased(laravel: true)
     ->withCache(
         cacheDirectory: '/tmp/rector',
         cacheClass: FileCacheStorage::class,
